c685b855d2d69a37ded2f623e07f773d24d7dae4
Takeru Ohta <phjgt308@gmail.com>
2016-10-12 13:00:50 +0900

    Update install.el

--- install.el
+++ install.el
@@ -3,16 +3,26 @@
 (package-initialize)
 (package-refresh-contents)
 
+;; git
 (package-install 'magit)
+(package-install 'git-gutter)
+
+;; markdown
+(package-install 'markdown-mode)
+
+;; rust
 (package-install 'rust-mode)
 (package-install 'cargo)
 (package-install 'toml-mode)
 (package-install 'racer)
 (package-install 'company-racer)
-(package-install 'markdown-mode)
-(package-install 'git-gutter)
+
+;; helm
 (package-install 'helm)
 (package-install 'helm-git-grep)
 (package-install 'helm-ls-git)
 (package-install 'helm-ag)
 (package-install 'helm-swoop)
+
+;; erlang
+(package-install 'erlang)
