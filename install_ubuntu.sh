12b5861051e7b4beeb26a87b6d0b065206f4d810
Takeru Ohta <phjgt308@gmail.com>
2017-02-08 12:36:47 +0900

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -5,22 +5,7 @@ set -eu
 ##
 ## Basic
 ##
-sudo apt install emacs-nox git tmux
-
-##
-## Erlang
-##
-ERL_PACKAGE=esl-erlang_19.0-1~ubuntu~xenial_amd64.deb
-sudo apt install libwxbase3.0-0v5 libwxgtk3.0-0v5
-
-if ! which erl
-then
-    if ! [ -f $HOME/Downloads/$ERL_PACKAGE ]
-    then
-        curl https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/$ERL_PACKAGE -o $HOME/Downloads/$ERL_PACKAGE
-    fi
-    sudo dpkg -i $HOME/Downloads/$ERL_PACKAGE
-fi
+sudo apt install emacs-nox git tmux gcc make
 
 ##
 ## Rust
