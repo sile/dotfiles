09a27c0bfe3c92769bc715485929d3226dd43bcd
Takeru Ohta <phjgt308@gmail.com>
2019-10-03 22:59:19 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ rustup install nightly
 rustup component add rust-src
 rustup component add rustfmt
 rustup component add clippy
-cargo install ripgrep evalrs cargo-update cargo-kcov cargo-outdated cargo-readme tokei hb rofis evcxr_repl
+cargo install ripgrep evalrs cargo-update cargo-kcov cargo-tarpaulin cargo-outdated cargo-readme tokei hb rofis evcxr_repl
 cargo +nightly install racer
 
 ##
