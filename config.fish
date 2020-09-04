2cd7bf27a81b4a3b33886e36e95f451c68bb50a9
Takeru Ohta <phjgt308@gmail.com>
2020-09-04 20:16:31 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -13,7 +13,8 @@ if not ssh-add -l > /dev/null
 end
 
 set -x LD_LIBRARY_PATH (rustc --print sysroot)/lib:(rustc +nightly --print sysroot)/lib
-set -x DISPLAY localhost:0.0
+# set -x DISPLAY localhost:0.0  # for WSL1
+set -x DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0  # for WSL2
 set -x DOCKER_HOST tcp://localhost:2375
 
 if not ps aux | grep rofis | grep -v grep > /dev/null
