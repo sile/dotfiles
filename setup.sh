649e1b28d338b4e6986c6e165d97bd8b418eead5
Takeru Ohta <phjgt308@gmail.com>
2023-12-21 20:08:36 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ DOT_FILES=(.emacs .gitconfig .gitignore .tmux.conf)
 
 for f in ${DOT_FILES[@]}
 do
-    ln -fns $PWD/$f $HOME/$f
+    cp $PWD/$f $HOME/$f
 done
 
 mkdir -p ~/.config/fish/
-ln -fns config.fish ~/.config/fish/config.fish
+cp config.fish ~/.config/fish/config.fish
