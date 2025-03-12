95560f15f97b44bf2d3b813c25e560f138c91161
Takeru Ohta <phjgt308@gmail.com>
2025-03-12 22:35:59 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,17 +1,5 @@
 set -U fish_user_paths $HOME/.cargo/bin $HOME/.local/bin
 
-touch ~/.ssh-agent-info
-source ~/.ssh-agent-info > /dev/null
-ssh-add -l > /dev/null
-if [ $status = 2 ]
-    ssh-agent -c > ~/.ssh-agent-info
-    source ~/.ssh-agent-info
-end
-
-if not ssh-add -l > /dev/null
-    ssh-add
-end
-
 set -x EDITOR emacs
 
 # HTTP server
@@ -23,6 +11,7 @@ end
 alias eng="daberu --system 'Translate English to Japanese and show a refined English version.'"
 alias refine "daberu --system 'Please refine the code.'"
 alias fixtodo "daberu --system 'When resolving TODOs, provide the completed code in plain text format without any markdown formatting, code block symbols, or additional commentary surrounding the code. The output should be ready for direct copy-paste into the target source code including sorrounding lines.'"
+alias autocommit "git commit -m (git diff --cached | daberu --system 'Generate one-line commit message from the git diff result')"
 set -x DABERU_MODEL "claude-3-7-sonnet-latest"
 set -x DABERU_ONESHOT_LOG_PATH "$HOME/tmp/daberu.jsonl"
 
